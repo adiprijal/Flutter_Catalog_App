875ac90999f1df7746db2533d98e9bf5fc1df57f
adiprijal <rizalaryan2@gmail.com>
2026-06-22 19:08:08 +0545

    Add LoginPage widget and update routing in main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog_app/pages/home_page.dart';
+import 'package:flutter_catalog_app/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,9 +10,22 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
+  // build method is called when the widget is inserted into the widget tree. It describes the part of the user interface represented by this widget.
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage()
+      // theme is a property of MaterialApp that defines the colors and fonts to use for the app. It is used to customize the look and feel of the app.
+      theme: ThemeData(
+        // primaryColor: Colors.grey,
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: HomePage(),
+      // routes is a property of MaterialApp that defines the routes for the app. It is a map that maps route names to the widget that should be displayed for that route. When the user navigates to a route, the corresponding widget is displayed.
+      routes: {
+        // The "/" route is the default route that is displayed when the app is launched. It is defined as a key in the routes map, and its value is a function that returns the widget to be displayed for that route. In this case, it returns an instance of HomePage.
+        // But as we have already defined the home property of MaterialApp to HomePage, we don't need to define the "/" route in the routes map. The home property takes precedence over the routes map for the default route. If we define both the home property and the "/" route in the routes map, the home property will be used for the default route, and the "/" route in the routes map will be ignored. Therefore, it is not necessary to define the "/" route in the routes map if we have already defined the home property.
+        // "/": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("Login Page")),
+      ),
+      body: Center(
+        child: Text("Login"),
+      ),
+    );
+  }
+}
