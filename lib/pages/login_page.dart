7298f7b0f4c79561b777abb5dc19e30b1343ca90
adiprijal <rizalaryan2@gmail.com>
2026-06-25 21:56:44 +0545

    Refactor LoginPage layout and enhance button animation with Material and InkWell

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -17,7 +17,10 @@ class _LoginPageState extends State<LoginPage> {
       child: SingleChildScrollView(
         child: Column(
           children: [
-            Image.asset("assets/images/enter-password.png", height: 200),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 60, 0, 0),
+              child: Image.asset("assets/images/enter-password.png", height: 200),
+            ),
             SizedBox(height: 20),
             Text("Login", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
             SizedBox(height: 20),
@@ -44,33 +47,30 @@ class _LoginPageState extends State<LoginPage> {
 
                   SizedBox(height: 20),
 
-                  InkWell(
-                    // async function to implement the delay for the animation of the button
-                    onTap: () async {
-                      // Navigator.pushNamed(context, MyRoutes.homeRoute);
-                      setState(() {
-                        changeButton = true;
-                      });
-
-                      // await implement some delay to show the animation of the button
-                      await Future.delayed(Duration(milliseconds: 600));
-                      Navigator.pushNamed(context, MyRoutes.homeRoute);
-                    },
-                    // AnimatedContainer to implement the animation of the button
-                    child: AnimatedContainer(
-                      // duration of the animation of the button, can be measured in milliseconds, seconds, minutes, hours, days
-                      duration: Duration(milliseconds: 600),
-                      // width value changes based on the value of changeButton, if changeButton is true, width is 40, else width is 120
-                      width: changeButton? 40: 120,
-                      height: 40,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.all( Radius.circular(24)),
-                      ),
-                      child: changeButton? Icon(Icons.done, color: Colors.white) : Text(
-                        "Login",
-                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
+                  Material(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(changeButton? 20: 8),
+                    child: InkWell(
+                      onTap: () async {
+                        setState(() {
+                          changeButton = true;
+                        });
+                    
+                        await Future.delayed(Duration(milliseconds: 600));
+                        await Navigator.pushNamed(context, MyRoutes.homeRoute);
+                        setState(() {
+                          changeButton = false;
+                        });
+                      },
+                      child: AnimatedContainer(
+                        duration: Duration(milliseconds: 600),
+                        width: 120,
+                        height: 40,
+                        alignment: Alignment.center,
+                        child: changeButton? Icon(Icons.done, color: Colors.white) : Text(
+                          "Login",
+                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white)
+                        ),
                       ),
                     ),
                   ),
