13dcf47bb869fe56a5ceb2b29db0bfc6495dd869
adiprijal <rizalaryan2@gmail.com>
2026-06-29 20:07:39 +0545

    Refactor HomePage to use ListView.builder for efficient item rendering of dummyList

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,27 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog_app/models/catalog.dart';
 import 'package:flutter_catalog_app/widgets/drawer.dart';
+import 'package:flutter_catalog_app/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Catalog App")
-      ),
+      appBar: AppBar(title: Text("Catalog App")),
       drawer: MyDrawer(),
-      body: Container(
-        color: Colors.white,
-        width: double.infinity,
-        child: Column(
-          children: [
-            Text(
-              "Catalog",
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
+      // ListView.builder() is used to create a scrollable list of items. It takes an itemCount and an itemBuilder function that defines how each item in the list should be built.
+      // It only builds the items that are visible on the screen, which makes it efficient for large lists.
+      body: ListView.builder(
+        itemCount: dummyList.length,
+        itemBuilder: (context, index) {
+          return ItemsWidget(item: dummyList[index]);
+        },
       ),
     );
   }
