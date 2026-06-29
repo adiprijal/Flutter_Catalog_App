7e059ecf5387d659a26363253445348f05d76cd4
adiprijal <rizalaryan2@gmail.com>
2026-06-29 23:54:33 +0545

    Refactor HomePage to use GridView for item display

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,7 +1,7 @@
 // Catalog Item Model
 
 class CatalogModel {
-  static List<Item> items = [];
+  static List<Item> ?items;
 }
 
 class Item {
@@ -21,9 +21,6 @@ class Item {
     required this.image,
   });
 
-// The `fromMap` factory constructor is used to create an instance of the `Item` class from a map (typically a JSON object). This is useful when you want to convert data received from an API or a local JSON file into Dart objects that can be used in your application. The constructor takes a `Map<String, dynamic>` as input and assigns the values from the map to the corresponding fields of the `Item` class.
-// All the final fields must be initialized in the constructor, and since they are marked as `required`, the constructor ensures that all necessary data is provided when creating an instance of the `Item` class.
-// factory constructors are a way to provide more control over the instantiation of a class. They can return instances of the class or even instances of subclasses, and they can contain logic to determine what instance to return. In this case, the `fromMap` factory constructor is used to create an `Item` instance from a map, which is a common pattern when working with data in Flutter applications.
   factory Item.fromMap(Map<String, dynamic> map) {
     return Item(
       id: map["id"],
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,8 @@
-// convert package is used to convert data between different formats, such as JSON and Dart objects. In this case, it is used to decode the JSON data loaded from the local file system into a Dart object that can be used in the app.
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
-// services.dart is used to load assets from the local file system, such as JSON files, images, and other resources. In this case, it is used to load a JSON file containing product data for the catalog app.
 import 'package:flutter/services.dart';
 import 'package:flutter_catalog_app/models/catalog.dart';
 import 'package:flutter_catalog_app/widgets/drawer.dart';
-import 'package:flutter_catalog_app/widgets/item_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -23,12 +19,12 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<void> loadData() async {
-    // Giving a delay of 2 seconds to simulate a loading time for the data. This is useful for testing the loading state of the app and ensuring that the UI behaves correctly while data is being fetched.
-    await Future.delayed(Duration(seconds: 2));
     var catalogJson = await rootBundle.loadString("assets/files/products.json");
     var decodedData = jsonDecode(catalogJson);
     var productsData = decodedData["products"];
-    CatalogModel.items = List.from(productsData).map((item) => Item.fromMap(item)).toList();
+    CatalogModel.items = List.from(
+      productsData,
+    ).map((item) => Item.fromMap(item)).toList();
 
     setState(() {});
   }
@@ -38,15 +34,41 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(title: Text("Catalog App")),
       drawer: MyDrawer(),
-      // Check if the items list is not null and not empty, then display the list of items using ListView.builder. If the items list is null or empty, display a CircularProgressIndicator to indicate that data is being loaded.
-      body: (CatalogModel.items != null && CatalogModel.items.isNotEmpty) ? ListView.builder(
-        itemCount: CatalogModel.items.length,
-        itemBuilder: (context, index) {
-          return ItemsWidget(item: CatalogModel.items[index]);
-        },
-      ) : Center(
-        child: CircularProgressIndicator(),
-      ),
+      body: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
+          ? GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 16,
+                crossAxisSpacing: 16,
+              ),
+              itemBuilder: (context, index) {
+                final item = CatalogModel.items![index];
+                return Card(
+                  clipBehavior: Clip.antiAlias,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: GridTile(
+                    header: Container(
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple
+                      ),
+                      child: Text(item.name, textAlign: TextAlign.center, style: TextStyle(color: Colors.white)),
+                    ),
+                    footer: Container(
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200]
+                      ),
+                      child: Text("\$${item.price}", textAlign: TextAlign.center)),
+                    child: Image.network(item.image),
+                  ),
+                );
+              },
+              itemCount: CatalogModel.items!.length,
+            )
+          : Center(child: CircularProgressIndicator()),
     );
   }
 }
