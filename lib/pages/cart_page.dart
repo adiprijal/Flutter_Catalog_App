1dcd76c14a0db318c8a64cb68441e1556a8f5bbb
adiprijal <rizalaryan2@gmail.com>
2026-07-01 11:48:24 +0545

    Updated CartPage

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog_app/widgets/buy_button.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class CartPage extends StatelessWidget {
@@ -11,7 +12,53 @@ class CartPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: "Cart".text.color(Theme.of(context).primaryColor).make(),
-      )
+      ),
+      body: Column(children: [_CartList().expand(), Divider(), _CartTotal()]),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  const _CartList();
+
+  @override
+  State<_CartList> createState() => __CartListState();
+}
+
+class __CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Icon(Icons.done),
+          trailing: IconButton(
+            icon: Icon(Icons.remove_circle_outline),
+            onPressed: () {},
+          ),
+          title: "Item 1".text.make(),
+        );
+      },
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  const _CartTotal();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\$999".text.xl2.color(Theme.of(context).primaryColor).make(),
+          18.widthBox,
+          BuyButton().wh(100, 36),
+        ],
+      ),
     );
   }
 }
